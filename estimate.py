17d3111262e99bebb42a9fbbc2ac3e2fd1f890bf
Deepak64bit <deepak_b190403cs@nitc.ac.in>
2021-05-29 21:01:48 +0530

    created wallis function

--- estimate.py
+++ estimate.py
@@ -30,3 +30,10 @@ class TestMC(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main()
+
+def wallis(n):
+    pi=1
+    for i in range (1,n+1):
+        pi*= (4*(i**2))/(4*(i**2)-1)
+    pi*=2
+    return pi
